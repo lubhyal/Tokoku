6819366dc63d67831af91a1a3041df2496f4485f
kusen abdullah <kusena322@gmail.com>
2021-09-24 17:46:50 +0700

    remove unsed code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 class HomeController extends Controller
 {
     public function index()
