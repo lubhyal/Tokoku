2e22e5475f30210b8d3e55d936f18c78af268345
lubhyal <lubhyal1@gmail.com>
2021-11-15 01:40:07 +0700

    Perubahan pada UI Admin

--- resources/views/admin/addstock.blade.php
+++ resources/views/admin/addstock.blade.php
@@ -39,7 +39,7 @@
 
         </div>
         
-        <button type="submit" class="btn btn-success w-100">TAMBAHKAN STOK</button>
+        <button type="submit" class="btn btn-success w-100 animation-on-hover">TAMBAHKAN STOK</button>
     
     </form>
 
